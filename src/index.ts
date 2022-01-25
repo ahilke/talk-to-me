0d1f2c72e29e2f829531f827b17edcf8e7c78a50
Arno Hilke <arno.hilke@tum.de>
2022-01-25 22:54:29 +0100

    Add Basic Speech Synthesis

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,41 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList; // not sup
 // @ts-ignore
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
+// TODO: synthesis seems to be broken on reload
+// TODO: how to stop recognition to pick up synthesized voice
+
+/**
+ * Synthesis
+ */
+
+let availableVoices: SpeechSynthesisVoice[] = [];
+
+const synthesis = window.speechSynthesis;
+function selectVoice() {
+    const voices = synthesis.getVoices();
+    console.debug("voices", voices);
+    availableVoices = voices.filter((voice) => voice.name === "Google UK English Male");
+    console.debug("filtered voices", availableVoices);
+}
+selectVoice();
+
+// not defined in firefox
+// in chrome, we need to wait until list of voices is fetched from Google's servers
+// privacy? offline? can we use a local voice?
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = selectVoice;
+}
+
+function speak(text: string) {
+    const utterance = new SpeechSynthesisUtterance(`${text}?`);
+    utterance.voice = availableVoices[0]; // handle empty array case (TS rule?)
+    synthesis.speak(utterance);
+}
+
+/**
+ * Recognition
+ */
+
 const recognition = new SpeechRecognition();
 const speechRecognitionList = new SpeechGrammarList();
 
@@ -24,6 +59,8 @@ recognition.onresult = (event: any) => {
     console.log("Result:" + result[0][0].transcript);
     console.debug("Alternatives:", result);
     console.debug("Confidence: " + result[0][0].confidence);
+
+    speak(result[0][0].transcript);
 
     // will be restarted as soon as it properly ended
     // otherwise, results are accumulated over multiple spoken phrases
